6bf6964754dcc67fbee714af9f749e116f6728e9
UrLocalIndianKid <122555211+UrLocalIndianKid@users.noreply.github.com>
2023-01-31 11:52:56 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        # # # # #
+        # . . . #
+        `)
+    Player_A += 1
+    Update_Score()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    Ties += 1
+    Update_Score()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # . # . .
+        # . . # .
+        # # # . .
+        `)
+    Player_B += 1
+    Update_Score()
+})
+input.onGesture(Gesture.Shake, function () {
+    Reset()
+})
 function Update_Score () {
     OLED.clear()
     OLED.writeStringNewLine("Player A: " + Player_A)
@@ -8,15 +44,18 @@ function Update_Score () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds: " + Rounds)
 }
+function Reset () {
+    OLED.init(128, 64)
+    Player_A = 0
+    Player_B = 0
+    Ties = 0
+    Rounds = 0
+    OLED.writeStringNewLine("PLAY THE GAME or else")
+    basic.pause(2000)
+    Update_Score()
+}
 let Rounds = 0
-let Ties = 0
 let Player_B = 0
+let Ties = 0
 let Player_A = 0
-OLED.init(128, 64)
-Player_A = 0
-Player_B = 0
-Ties = 0
-Rounds = 0
-OLED.writeStringNewLine("PLAY THE GAME or else")
-basic.pause(2000)
-Update_Score()
+Reset()
